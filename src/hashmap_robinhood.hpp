51868d8e8b0baf368562d4f49eeae3c7ed60db58
AlexPapantoniou <alexandrospapantoniou95@gmail.com>
2025-11-04 15:33:43 +0200

    Robinhood optimization

--- src/hashmap_robinhood.hpp
+++ src/hashmap_robinhood.hpp
@@ -8,6 +8,7 @@
 #include <optional>
 #include <vector>
 #include <memory>
+#include <type_traits>
 
 template<typename Key, typename Value>
 class HashMapRobinhood {
@@ -17,20 +18,30 @@ private:
         Value value;
         unsigned int PSL;
     };
+
     size_t _capacity;
     std::vector<std::optional<Node>> buckets;
     size_t _size;
     unsigned int max_PSL;
     std::hash<Key> hasher;
 
-    inline size_t mask() const {
+    inline size_t mask() const noexcept {
         return _capacity - 1;
     }
 
-    static size_t next_power_of_two(size_t n) {
+    static size_t next_power_of_two(size_t n) noexcept {
         size_t p = 1;
         while (p < n) p <<= 1;
         return p;
+    }
+
+    static size_t mix_hash(size_t h) noexcept {
+        h ^= (h >> 33);
+        h *= 0xff51afd7ed558ccdULL;
+        h ^= (h >> 33);
+        h *= 0xc4ceb9fe1a85ec53ULL;
+        h ^= (h >> 33);
+        return h;
     }
 
     void rehash() {
@@ -40,11 +51,14 @@ private:
         _capacity <<= 1;
         _size = 0;
         max_PSL = 0;
-        buckets.assign(_capacity, std::nullopt);
+        buckets.clear();
+        buckets.resize(_capacity);
 
         for (std::size_t i = 0; i < old_capacity; i++) {
-            if (old_buckets[i].has_value()) {
-                emplace(old_buckets[i]->key, old_buckets[i]->value);
+            auto& bucket = old_buckets[i];
+
+            if (bucket.has_value()) {
+                emplace(std::move(bucket->key), std::move(bucket->value));
             }
         }
     }
@@ -56,20 +70,8 @@ public:
         size_t index;
 
         void advance_to_valid() {
-            if (!map) {
-                view.reset();
-                return;
-            }
-            while (index < map->_capacity && !map->buckets[index].has_value()) {
+            while (map && index < map->_capacity && !map->buckets[index].has_value()) {
                 index++;
-            }
-            if (index < map->_capacity && map->buckets[index].has_value()) {
-                view = std::make_unique<PairView>(
-                    map->buckets[index]->key,
-                    map->buckets[index]->value);
-            }
-            else {
-                view.reset();
             }
         }
 
@@ -78,31 +80,25 @@ public:
             advance_to_valid();
         }
 
-        struct PairView {
-            const Key& first;
-            Value& second;
-            PairView(const Key& f, Value& s) : first(f), second(s) {}
-        };
-
-        mutable std::unique_ptr<PairView> view;
-
     public:
         using iterator_category = std::forward_iterator_tag;
         using value_type = std::pair<const Key, Value>;
         using difference_type = std::ptrdiff_t;
-        using pointer = PairView*;
-        using reference = PairView&;
+        using pointer = value_type*;
+        using reference = value_type&;
 
         iterator()
             : map(nullptr), index(0) {
         }
 
         reference operator*() const {
-            return *view;
+            auto& node = *map->buckets[index];
+            return *reinterpret_cast<value_type*>(&node);
         }
 
         pointer operator->() const {
-            return &*view;
+            auto& node = *map->buckets[index];
+            return reinterpret_cast<value_type*>(&node);
         }
 
         iterator& operator++() {
@@ -135,40 +131,88 @@ public:
 
     ~HashMapRobinhood() = default;
 
-    size_t size() const {
+    size_t size() const noexcept {
         return _size;
     }
 
-    size_t capacity() const {
+    size_t capacity() const noexcept {
         return _capacity;
     }
 
-    bool empty() const {
+    bool empty() const noexcept {
         return _size == 0;
     }
 
+    void reserve(size_t expected) {
+        _capacity = next_power_of_two(static_cast<size_t>(expected * 1.5));
+        buckets.assign(_capacity, std::nullopt);
+    }
+
     bool emplace(const Key& key, const Value& value) {
-        if (static_cast<double>(_size) / static_cast<double>(_capacity) >= 0.5) {
+        if ((_size + 1) * 4 >= _capacity * 3) {
             rehash();
         }
 
-        size_t index = hasher(key) & mask();
+        size_t hash = mix_hash(hasher(key));
+        const size_t cap_mask = mask();
+        size_t index = hash & cap_mask;
         unsigned int PSL = 0;
 
         Key cur_key = key;
         Value cur_value = value;
 
         while (true) {
-            size_t i = (index + PSL) & mask();
-            if (!buckets[i].has_value()) {
-                buckets[i].emplace(Node{ std::move(cur_key), std::move(cur_value), PSL });
+            size_t i = (index + PSL) & cap_mask;
+            auto& bucket = buckets[i];
+
+            if (!bucket.has_value()) {
+                bucket.emplace(Node{ std::move(cur_key), std::move(cur_value), PSL });
                 _size++;
                 max_PSL = std::max(PSL, max_PSL);
                 return true;
             }
-            else if (PSL > buckets[i]->PSL) {
-                Node old_node = std::move(*buckets[i]);
-                buckets[i].emplace(Node{ std::move(cur_key), std::move(cur_value), PSL });
+            else if (PSL > bucket->PSL) {
+                Node old_node = std::move(*bucket);
+                bucket.emplace(Node{ std::move(cur_key), std::move(cur_value), PSL });
+                cur_key = std::move(old_node.key);
+                cur_value = std::move(old_node.value);
+                PSL = old_node.PSL + 1;
+                max_PSL = std::max(PSL, max_PSL);
+            }
+            else {
+                PSL++;
+            }
+        }
+
+        return false;
+    }
+
+    bool emplace(Key&& key, Value&& value) {
+        if ((_size + 1) * 4 >= _capacity * 3) {
+            rehash();
+        }
+
+        size_t hash = mix_hash(hasher(key));
+        const size_t cap_mask = mask();
+        size_t index = hash & cap_mask;
+        unsigned int PSL = 0;
+
+        Key cur_key = std::move(key);
+        Value cur_value = std::move(value);
+
+        while (true) {
+            size_t i = (index + PSL) & cap_mask;
+            auto& bucket = buckets[i];
+
+            if (!bucket.has_value()) {
+                bucket.emplace(Node{ std::move(cur_key), std::move(cur_value), PSL });
+                _size++;
+                max_PSL = std::max(PSL, max_PSL);
+                return true;
+            }
+            else if (PSL > bucket->PSL) {
+                Node old_node = std::move(*bucket);
+                bucket.emplace(Node{ std::move(cur_key), std::move(cur_value), PSL });
                 cur_key = std::move(old_node.key);
                 cur_value = std::move(old_node.value);
                 PSL = old_node.PSL + 1;
@@ -183,16 +227,24 @@ public:
     }
 
     iterator find(const Key& key) {
-        size_t index = hasher(key) & mask();
+        size_t hash = mix_hash(hasher(key));
+        const size_t cap_mask = mask();
+        size_t index = hash & cap_mask;
         unsigned int PSL = 0;
 
+
         while (PSL <= max_PSL) {
-            size_t i = (index + PSL) & mask();
-            if (!buckets[i].has_value()) {
+            size_t i = (index + PSL) & cap_mask;
+            const auto& bucket = buckets[i];
+
+            if (!bucket.has_value()) {
                 return end();
             }
-            if (buckets[i]->key == key) {
+            if (bucket->key == key) {
                 return iterator(this, i);
+            }
+            if (bucket->PSL < PSL) {
+                return end();
             }
 
             PSL++;
