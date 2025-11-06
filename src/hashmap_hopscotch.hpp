603c1786920d759096f93954e7892f1bc0d56fa0
sdi2200137 <sdi2200137@uoa.gr>
2025-11-06 17:31:29 +0200

    slight optimization for hopscotch

--- src/hashmap_hopscotch.hpp
+++ src/hashmap_hopscotch.hpp
@@ -6,6 +6,7 @@
 #include <optional>
 #include <vector>
 #include <memory>
+#include <iostream>
 
 template<typename Key, typename Value>
 class HashMapHopscotch {
@@ -21,11 +22,11 @@ private:
     size_t _size;
     std::hash<Key> hasher;
 
-    inline size_t mask() {
+    inline size_t mask() const noexcept{
         return _capacity - 1;
     }
 
-    static size_t next_power_of_two(size_t n) {
+    static size_t next_power_of_two(size_t n) noexcept{
         size_t p = 1;
         while (p < n) p <<= 1;
         return p;
@@ -33,7 +34,6 @@ private:
 
     void rehash() {
         std::vector<std::optional<Node>> old_buckets = std::move(buckets);
-        std::vector<std::uint16_t> old_hop_info = std::move(hop_info);
         size_t old_capacity = _capacity;
 
         _capacity <<= 1;
@@ -42,10 +42,20 @@ private:
         hop_info.assign(_capacity, 0);
 
         for (size_t i = 0; i < old_capacity; i++) {
-            if (old_buckets[i].has_value()) {
-                emplace(old_buckets[i]->key, old_buckets[i]->value);
+            auto& bucket = old_buckets[i];
+            if (bucket.has_value()) {
+                emplace(std::move(bucket->key), std::move(bucket->value));
             }
         }
+    }
+
+    static size_t mix_hash(size_t h) noexcept {
+        h ^= (h >> 33);
+        h *= 0xff51afd7ed558ccdULL;
+        h ^= (h >> 33);
+        h *= 0xc4ceb9fe1a85ec53ULL;
+        h ^= (h >> 33);
+        return h;
     }
 
 public:
@@ -55,18 +65,8 @@ public:
         size_t index;
 
         void advance_to_valid() {
-            if (!map) {
-                view.reset();
-                return;
-            }
-            while (index < map->_capacity && !map->buckets[index].has_value()) index++;
-            if (index < map->_capacity && map->buckets[index].has_value()) {
-                view = std::make_unique<PairView>(
-                    map->buckets[index]->key,
-                    map->buckets[index]->value);
-            }
-            else {
-                view.reset();
+            while (map && index < map->_capacity && !map->buckets[index].has_value()) {
+                index++;
             }
         }
 
@@ -75,20 +75,14 @@ public:
             advance_to_valid();
         }
 
-        struct PairView {
-            const Key& first;
-            Value& second;
-            PairView(const Key& f, Value& s) : first(f), second(s) {}
-        };
-
-        mutable std::unique_ptr<PairView> view;
 
     public:
         using iterator_category = std::forward_iterator_tag;
         using value_type = std::pair<const Key, Value>;
         using difference_type = std::ptrdiff_t;
-        using pointer = PairView*;
-        using reference = PairView&;
+        using pointer = value_type*;
+        using reference = value_type&;
+
 
         iterator()
             : map(nullptr),
@@ -96,11 +90,13 @@ public:
         }
 
         reference operator*() const {
-            return *view;
+            auto& node = *map->buckets[index];
+            return *reinterpret_cast<value_type*>(&node);
         }
 
         pointer operator->() const {
-            return &*view;
+            auto& node = *map->buckets[index];
+            return reinterpret_cast<value_type*>(&node);
         }
 
         iterator& operator++() {
@@ -129,54 +125,69 @@ public:
         buckets(_capacity),
         neighbours(neighb_init),
         hop_info(_capacity, 0),
-        _size(0){
+        _size(0) {
     }
 
     ~HashMapHopscotch() = default;
 
-    size_t size() const {
+    size_t size() const noexcept{
         return _size;
     }
 
-    size_t capacity() const {
+    size_t capacity() const noexcept{
         return _capacity;
     }
 
-    bool empty() const {
+    bool empty() const noexcept{
         return _size == 0;
     }
 
+    unsigned int neighbours_size() const noexcept{
+        return neighbours;
+    }
+
+    void reserve(size_t expected) {
+        _capacity = next_power_of_two(static_cast<size_t>(expected * 1.5));
+        buckets.assign(_capacity, std::nullopt);
+        hop_info.assign(_capacity, 0);
+    }
+
     bool emplace(const Key& key, const Value& value) {
-        if (_size >= _capacity / 2) {
+        // std::cout << "CAPACITY" << _capacity << std::endl;
+        // std::cout << "NEIGHBOURS" << neighbours << std::endl;
+        
+        if ((_size + 1) * 4 >= _capacity * 3) {
             rehash();
         }
 
-        size_t index = hasher(key) & mask();
+        size_t hash = mix_hash(hasher(key));
+        const size_t cap_mask = mask();
+        size_t index = hash & cap_mask;
 
-        if (hop_info[index] == (1u << neighbours) - 1) {
+        if (hop_info[index] == ((1u << neighbours)) - 1) {
             rehash();
             return emplace(key, value);
         }
 
         size_t j = index;
         while (buckets[j].has_value()) {
-            j = (j + 1) & mask();
+            j = (j + 1) & cap_mask;
         }
 
-        while (((j - index) & mask()) >= neighbours) {
+        while (((j - index) & cap_mask) >= neighbours) {
             bool moved = false;
 
             for (size_t offset = neighbours - 1; offset > 0; offset--) {
-                size_t k = (j - offset) & mask();
+                size_t k = (j - offset) & cap_mask;
 
                 uint16_t hop = hop_info[k];
                 for (size_t bit = 0; bit < neighbours; bit++) {
                     if (hop & (1u << bit)) {
-                        size_t y_index = (k + bit) & mask();
-                        if (((j - y_index) & mask()) < neighbours) {
+                        size_t y_index = (k + bit) & cap_mask;
+                        if (((j - y_index) & cap_mask) < neighbours) {
                             buckets[j] = buckets[y_index];
                             hop_info[k] &= ~(1u << bit);
-                            hop_info[k] |= (1u << ((j - k) & mask()));
+                            hop_info[k] |= (1u << ((j - k) & cap_mask));
                             buckets[y_index].reset();
                             j = y_index;
                             moved = true;
@@ -195,19 +206,22 @@ public:
         }
 
         buckets[j].emplace(Node{ std::move(key), std::move(value) });
-        hop_info[index] |= (1u << ((j - index) & mask()));
+        hop_info[index] |= (1u << ((j - index) & cap_mask));
         _size++;
         return true;
     }
 
     iterator find(const Key& key) {
-        size_t index = hasher(key) & mask();
+        size_t hash = mix_hash(hasher(key));
+        const size_t cap_mask = mask();
+        size_t index = hash & cap_mask;
         uint16_t hop = hop_info[index];
 
         for (size_t bit = 0; bit < neighbours; bit++) {
             if (hop & (1u << bit)) {
-                size_t j = (index + bit) & mask();
-                if (buckets[j].has_value() && buckets[j]->key == key) {
+                size_t j = (index + bit) & cap_mask;
+                auto& bucket = buckets[j];
+                if (bucket.has_value() && bucket->key == key) {
                     return iterator(this, j);
                 }
             }
